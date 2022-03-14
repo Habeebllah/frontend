d90bb3ff9276c66ba72221e1bcae19cafb7bd871
Habeebllah <lasisihabeeb67@gmail.com>
2022-03-14 13:10:54 +0100

    Javascripts Class 01

---
+++ javascript/main.js
@@ -0,0 +1,65 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+
+console.log(z+x)
+
+let c = 1;
+let b = 8;
+let u = c + b;
+console.log(u)
+
+
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total);
+
+
+let Age = 5;
+console.log(Age)
+
+
+let vary;
+vary = 4;
+console.log(vary)
+
+let student1 = "Tolu", student2 = 'Bunmi';
+console.log(student1)
+console.log(student2)
+
+let weight = 5.8;
+console.log(weight)
+
+let avaliable = false;
+console.log(avaliable)
+
+let carName = "Volvo"; 
+
+
+// var question = prompt("Enter your name: ");
+// document.getElementById("demo").innerHTML = question;
+
+/*
+write a program that ask a user of ask a user of his / her name, age, address and 
+phone number then your program should display the values on the html webpage.
+*/
+
+// alert("Welcome to ATC " + question)
+
+let num1 = 9, num2 = 10;
+console.log("*************")
+console.log(num1 * num2)
+console.log(num1 - num2)
+console.log(num1 / num2)
+console.log(num1 + num2)
+console.log(num1 ** num2)
+console.log(num2 % num1)
+
+/*
+ASSIGNMENT
+
+Write a program with javascript to calculate the number of seconds in a week
+then display your answer on the html webpage
+*/
+
