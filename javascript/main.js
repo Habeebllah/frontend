3006eaa18bb316bdeb3dbace3e64a4cba38d8629
Habeebllah <lasisihabeeb67@gmail.com>
2022-03-15 13:11:47 +0100

    15/03/2022

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,4 @@
+/*
 var x = 5;
 var y = 6;
 var z = x + y;
@@ -40,10 +41,10 @@ let carName = "Volvo";
 // var question = prompt("Enter your name: ");
 // document.getElementById("demo").innerHTML = question;
 
-/*
+
 write a program that ask a user of ask a user of his / her name, age, address and 
 phone number then your program should display the values on the html webpage.
-*/
+
 
 // alert("Welcome to ATC " + question)
 
@@ -56,6 +57,8 @@ console.log(num1 + num2)
 console.log(num1 ** num2)
 console.log(num2 % num1)
 
+*/
+
 /*
 ASSIGNMENT
 
@@ -63,3 +66,76 @@ Write a program with javascript to calculate the number of seconds in a week
 then display your answer on the html webpage
 */
 
+
+// 15/03/2022
+
+let number1 = 5
+let number2 = 10
+console.log(number2 % number1)
+
+// Object
+
+let grade = {
+    math: 90,
+    english: 100,
+    chemistry: 50,
+    physics: 100
+}
+
+
+console.log(grade.math)
+
+
+function addition() {
+    return demo.innerHTML = "Hello World"
+  }
+
+
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+        return this.firstName + " " + this.lastName + " " + this.id;
+    }
+};
+
+person["Color"] = "White"
+person['firstName'] = "Habeeb"
+console.log(person)
+
+
+const cars = ["Saab", "Volvo", "BMW"]
+const fruits = [
+    "mango", 
+    "pawapaw", 
+    "orange", 
+    "banana", 
+    "apple"
+]
+console.log(fruits[2] + " " + fruits[0])
+fruits.push("Pineapple")
+fruits.shift()
+fruits.unshift("Pear")
+delete fruits[0];
+console.log(fruits.length)
+
+const bike = new Array("Power Bike", "Boosteeh Bike", "Suzuki", "Honda")
+console.log(bike[1])
+
+const new_fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = new_fruits.toString();
+
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+const myChildren = myGirls.concat(myBoys);
+console.log(myChildren)
+
+const fruit = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruit.slice(3);
+console.log(citrus)
+
+const fruis = ["Banana", "Orange", "Apple", "Mango"];
+fruis.splice(2, 2, "Lemon", "Kiwi");
+console.log(fruis)
