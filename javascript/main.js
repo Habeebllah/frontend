82c91222fa5106c143756626bf19091a516b1ebb
Habeebllah <lasisihabeeb67@gmail.com>
2022-03-17 12:25:08 +0100

    17/03/2022

--- javascript/main.js
+++ javascript/main.js
@@ -68,7 +68,7 @@ then display your answer on the html webpage
 
 
 // 15/03/2022
-
+/*
 let number1 = 5
 let number2 = 10
 console.log(number2 % number1)
@@ -139,3 +139,69 @@ console.log(citrus)
 const fruis = ["Banana", "Orange", "Apple", "Mango"];
 fruis.splice(2, 2, "Lemon", "Kiwi");
 console.log(fruis)
+*/
+
+let y = 4
+let z = 15
+if (y == z ){
+    console.log("Hello World!")
+}
+
+else if (y > z){
+    console.log("Greater")
+}
+
+else if (y < z){
+    console.log("Less")
+}
+else{
+    console.log("Plan B executed")
+}
+
+
+
+alert("Welcome to my website kindly sign up before you can login to access your dashboard")
+
+let username, password
+username = prompt("Enter your prefered username")
+password = prompt("Enter your prefered password")
+
+const data = {
+    "username": username,
+    "password": password
+}
+
+alert("Signup successful, kindly proceed to login by clicking ok")
+let loginUsername = prompt("Enter username")
+let loginPassword = prompt("Enter password")
+
+if ((data["username"] == loginUsername) && (data["password"] == loginPassword)){
+    alert("Login Successful")
+    user.innerHTML = loginUsername
+}
+
+else if ((data["username"] == loginUsername) && (data["password"] != loginPassword)){
+    alert("Password Incorrect")
+}
+
+else if ((data["username"] != loginUsername) && (data["password"] != loginPassword)){
+    alert("Username and Password Incorrect")
+}
+
+/*
+Three friends went to eat in a resturant and the their total bill was
+N10,000. Write a program with javascript to calculate how much each of 
+them would pay if they are to leave 30% to their total bill.
+
+
+A store is making discount on sales, write a program to ask a user how much
+his or her purchase is. If the user purchase price is less than N2000 then
+the user should have 10% discount on his / her purchase, if the user purchase
+price is between N2000 and N5000 then the user should have 20% discount and if
+the purchase if above N5000 then the user should have 30% discount.
+
+Then your program should display the initial price, discount price and new
+purchase price after removing the discount price from it.
+
+NOTE: Ensure your display your output on the console and also the webpage.
+*/
